45a5e0b230fcef5299c37c1664473a66f244da10
Marcos Felix <marcosfelixcosta@gmail.com>
2024-04-26 21:11:38 -0300

    Projeto inicial cabeçalho 01

--- PhotoMax.cpp
+++ PhotoMax.cpp
@@ -1,9 +1,18 @@
+/*
+- Aplicação para tratamento de imagens
+
+- Analista: Marcos Felix
+- Data: 25/04/2024
+
+
+*/
+
 #include <iostream>
 using namespace std;
 
 int main(int argc, char* argv []) {
 	setlocale(LC_ALL,"pt_BR");
-	//cout << "Primeira função C++" << endl;
+	cout << "Aplicação C++ para tratamento de imagens" << endl;
 	for (int i = 0; i < argc; i++)
 	{
 		cout << argv[i] << endl;
